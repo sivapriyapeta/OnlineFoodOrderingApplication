bfed82faebd7fd81cffd9743463cde1f89504175
Sivapriya Peta <GD000009220@ups.com>
2024-06-09 16:01:18 +0200

    pushing further work for project

--- Scripts/LoadHtmlPage.js
+++ Scripts/LoadHtmlPage.js
@@ -12,7 +12,7 @@ function LoginOrResiterForm(ele){
         // loginForm.classList.remove("show");
         // loginForm.classList.remove("active");
         registerForm.classList.add("show");
-        registerForm.classList.add("active");
+        //registerForm.classList.add("active");
         registerForm.style.display = "block";
         loginForm.style.display = "none";
         
@@ -31,40 +31,65 @@ function LoginOrResiterForm(ele){
 //     }
 // }
 function loginFormValidation(x){
+    var formInputs = document.getElementsByTagName("input");
     if(x.id == "email"){
-        ValidateEmail(x.id);
+        ValidateEmail(x.id, formInputs);
     }
     else if(x.id == "password"){
-        ValidatePassword(x.id);
+        ValidatePassword(x.id, formInputs);
     }
 }
 
-function ValidateEmail(elementId){
+function ValidateEmail(elementId, formInputs){
     var emailId = document.getElementById(elementId);
     var emailRegExPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if(!emailRegExPattern.test(emailId.value) || emailId.value === ""){
         emailId.classList.add("inValid");
         emailId.classList.remove("valid");
         emailId.insertAdjacentHTML("afterend", `<span id="emailIdValidationMessage">Invalid Email!</span>`);
+        document.getElementById("loginButton").disabled = true;
     }
     else{
         emailId.classList.remove("inValid");
         emailId.classList.add("valid");
-        document.getElementById("emailIdValidationMessage").style.display = "none";
+        var errorMessage = document.getElementById("emailIdValidationMessage");
+        if(errorMessage!=null){
+            document.getElementById("emailIdValidationMessage").style.display = "none";
+        };
+        if(!formInputs.password.classList.contains("inValid") && formInputs.password.value.length > 8 ){
+            document.getElementById("loginButton").disabled = false;
+        }
     }
 }
 
-function ValidatePassword(elementId){
+function ValidatePassword(elementId, formInputs){
     var password = document.getElementById(elementId);
     if(password.value.length <= 8 || password.value === ""){
         password.classList.add("inValid");
         password.classList.remove("valid");
         password.insertAdjacentHTML("afterend", `<span id="passwordValidationMessage" style="display: none;">Invalid Password!</span>`);
         document.getElementById("passwordValidationMessage").style.display = "block";
+        document.getElementById("loginButton").disabled = true;
     }
     else{
         password.classList.remove("inValid");
         password.classList.add("valid");
-        document.getElementById("passwordValidationMessage").style.display = "none";
+        var errorMessage = document.getElementById("passwordValidationMessage");
+        if(errorMessage!=null){
+            document.getElementById("passwordValidationMessage").style.display = "none";
+        };
+        if(!formInputs.email.classList.contains("inValid") && formInputs.email.value.length > 0 ){
+            document.getElementById("loginButton").disabled = false;
+        }
     }
 }
+
+function loginSubmit(){
+    var loginName = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var submitButton = document.getElementById("loginButton");
+    document.getElementById("loginName").innerHTML = loginName;
+    document.getElementById('loginButton').insertAdjacentHTML('afterend', `<span id="successMessage" style="display: none;">Login Successful!</span>`)
+    document.getElementById("successMessage").style.display = "block";
+}
+
---
+++ Scripts/Nav.js
@@ -0,0 +1,5 @@
+// document.getElementById("mainContainer").onload = function() {loadNav()};
+
+function loadNav(){
+    document.getElementById("content").innerHTML = '<object type="text/html" data="Nav.html" ></object>';
+}
