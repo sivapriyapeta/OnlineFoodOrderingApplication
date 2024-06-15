efe98ad0b5584ab5a84323d21726646ab4fb3513
Sivapriya Peta <GD000009220@ups.com>
2024-06-15 19:29:44 +0200

    pushing more commits

--- Scripts/LoadHtmlPage.js
+++ Scripts/LoadHtmlPage.js
@@ -93,3 +93,87 @@ function loginSubmit(){
     document.getElementById("successMessage").style.display = "block";
 }
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const addCartToButtons = document.querySelectorAll('.add-to-cart');
+//     const cartItemCount = document.querySelector('.cart-icon span');
+//     const cartItemsList = document.querySelector('.cart-tems');
+//     const cartTotal = document.querySelector('.cart-total');
+//     const cartIcon = document.querySelector('.cart-icon');
+//     const sideBar = document.getElementById('sidebar');
+
+//     let cartItems = [];
+//     let totalAmount = 0;
+//     addCartToButtons.forEach((button, index) => {
+//         button.addEventListener('click', () =>{
+//             const item = {
+//                 name: document.querySelectorAll('.card .card-title')[index].textContent,
+//                 price: parseFloat(document.querySelectorAll('.price')[index].textContent.slice(1),
+//             ),
+//             quantity:1,
+//             };
+//             const existingItem = cartItems.find(
+//                 (cartItem) => cartItem.name === item.name,
+//             );
+//             if(existingItem){
+//                 existingItem.quantity++;
+//             }
+//             else{
+//                 cartItems.push(item);
+//             }
+
+//             totalAmount +=item.price;
+//             UpdateCartUI();
+//         });
+
+//         function UpdateCartUI(){
+//             updateCartItemCount(cartItems.length);
+//             updateCartItemList();
+//             updateCartTotal();
+//         }
+
+//         function updateCartItemCount(count){
+//             cartItemCount.textContent = count;
+//         }
+
+//         function updateCartItemList(){
+//             cartItemsList.innerHTML = '';
+//             cartItems.forEach((item, index) => {
+//                 const cartItem = document.createElement('div');
+//                 cartItem.classList.add('cart-item', 'individual-cart-item');
+//                 cartItem.innerHTML=`
+//                 <span>(${item.quantity}x)${item.name}</span>
+//                 <span class="cart-item-price">${(item.price * item.quantity).toFixed(2)}
+//                 <button class="remove-btn" data-index="${index}"><i class="fa fa-times" aria-hidden="true"></i></button>
+//                 </span>
+//                 `;
+
+//                 cartItemsList.append(cartItem);
+//             });
+//             const removeButtons = document.querySelectorAll('remove-item');
+//             removeButtons.forEach((button) => {
+//                 button.addEventListener('click', (event) => {
+//                     const index = event.target.dataset.index;
+//                     removeItemFromCart(index);
+//                 });
+//             });
+//         }
+//         function removeItemFromCart(index){
+//             const removeItem = cartItems.splice(index, 1)[0];
+//             totalAmount -= removeItem.price * removeItem.quantity;
+//             UpdateCartUI();
+//         }
+
+//         function UpdateCartTotal(){
+//             cartTotal.textContent = `$${totalAmount.toFixed(2)}`;
+//         }
+
+//         cartIcon.addEventListener('click', () => {
+//             sideBar.classList.toggle('open');
+//         });
+
+//         const closeButton = document.querySelector('sidebar-close');
+//         closeButton.addEventListener('click', () =>{
+//             sideBar.classList.remove('open');
+//         });
+//     });
+// });
